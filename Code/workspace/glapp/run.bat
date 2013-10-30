bcc84942921212915e08c337d9e384bb7a8030b1
Nate Schultz <nate.schultz@outlook.com>
2013-10-30 18:52:11 -0400

    Added jogl progress thus far

---
+++ Code/workspace/glapp/run.bat
@@ -0,0 +1,5 @@
+
+rem  For testing, two demos come with the glapp.jar installation:  
+rem          GLApp_Demo_HelloWorld and GLApp_DemoLight
+
+java  -cp .;glapp.jar;lwjgl.jar;lwjgl_util.jar;  gldemo.GLApp_DemoLight
