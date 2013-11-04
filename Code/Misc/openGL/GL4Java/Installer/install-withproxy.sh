838c2db4c1a652714c96a252e48ed40ddba05fd7
nschultz14 <nschultz14@localhost>
2013-11-04 16:34:56 -0500

    cleaned up repository

--- Code/Misc/openGL/GL4Java/Installer/install-withproxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z ${CLASSPATH:=""} ] ; then
-	CLASSPATH=GL4JInstaller.jar
-else
-	CLASSPATH=GL4JInstaller.jar:${CLASSPATH}
-fi
-export CLASSPATH
-exec java -Dhttp.proxyHost=192.168.0.1 -Dhttp.proxyPort=3128 GL4JInst
--- Code/Misc/openGL/GL4Java/Installer/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z ${CLASSPATH:=""} ] ; then
-	CLASSPATH=GL4JInstaller.jar
-else
-	CLASSPATH=GL4JInstaller.jar:${CLASSPATH}
-fi
-export CLASSPATH
-exec java GL4JInst
