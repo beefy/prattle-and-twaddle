9677dd7ed1edc080025678d32e32ffd58c8a532b
Nate Schultz <nate.schultz@outlook.com>
2013-12-01 11:59:19 -0500

    got texturing to work

--- Code/Pix/src/com/carnivorous_exports/pix/Terrain.java
+++ Code/Pix/src/com/carnivorous_exports/pix/Terrain.java
@@ -1,15 +1,48 @@
 package com.carnivorous_exports.pix;
 
+import static javax.media.opengl.GL.GL_LINEAR;
+import static javax.media.opengl.GL.GL_TEXTURE_2D;
+import static javax.media.opengl.GL.GL_TEXTURE_MAG_FILTER;
+import static javax.media.opengl.GL.GL_TEXTURE_MIN_FILTER;
 import static javax.media.opengl.GL2.GL_COMPILE;
 import static javax.media.opengl.GL2GL3.GL_QUADS;
 
+import java.io.IOException;
+
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLException;
+
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureCoords;
+import com.jogamp.opengl.util.texture.TextureIO;
 
 public class Terrain {
+
+	// for testing rotation
+	float tempRotX;
+
+	Texture texture;
+
+	// Texture image flips vertically. Shall use TextureCoords class to retrieve
+	// the top, bottom, left and right coordinates.
+	private float textureTop;
+	private float textureBottom;
+	private float textureLeft;
+	private float textureRight;
+
+	private String textureFileName = "images/nehe.png";
+	private String textureFileType = ".png";
+
+	private static float[][] boxColors = { // Bright: Red, Orange, Yellow,
+			// Green, Blue
+			{ 1.0f, 0.0f, 0.0f }, { 1.0f, 0.5f, 0.0f }, { 1.0f, 1.0f, 0.0f },
+			{ 0.0f, 1.0f, 0.0f }, { 0.0f, 1.0f, 1.0f } };
 
 	public int getCubeList(GL2 gl) {
 		// Build two lists, and returns handle for the first list
 		// create one display list
+
+		loadTexture(gl);
 
 		int base = gl.glGenLists(1);
 
@@ -17,53 +50,135 @@ public class Terrain {
 		int cubeDList = base;
 
 		gl.glNewList(cubeDList, GL_COMPILE);
+
+		// Enables this texture's target in the current GL context's state.
+		texture.enable(gl); // same as gl.glEnable(texture.getTarget());
+		// gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE,
+		// GL.GL_REPLACE);
+		
+		// Binds this texture to the current GL context.
+		texture.bind(gl); // same as gl.glBindTexture(texture.getTarget(),
+							// texture.getTextureObject());
+
 		gl.glBegin(GL_QUADS);
 
-		// Top-face
-		gl.glColor3f(0.0f, 1.0f, 0.0f); // green
-		gl.glVertex3f(1.0f, 1.0f, -1.0f);
-		gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-		gl.glVertex3f(-1.0f, 1.0f, 1.0f);
-		gl.glVertex3f(1.0f, 1.0f, 1.0f);
+		// Front Face
+		gl.glTexCoord2f(textureLeft, textureBottom);
+		gl.glVertex3f(-1.0f, -1.0f, 1.0f); // bottom-left of the texture and
+											// quad
+		gl.glTexCoord2f(textureRight, textureBottom);
+		gl.glVertex3f(1.0f, -1.0f, 1.0f); // bottom-right of the texture and
+											// quad
+		gl.glTexCoord2f(textureRight, textureTop);
+		gl.glVertex3f(1.0f, 1.0f, 1.0f); // top-right of the texture and quad
+		gl.glTexCoord2f(textureLeft, textureTop);
+		gl.glVertex3f(-1.0f, 1.0f, 1.0f); // top-left of the texture and quad
 
-		// Bottom-face
-		gl.glColor3f(1.0f, 0.5f, 0.0f); // orange
-		gl.glVertex3f(1.0f, -1.0f, 1.0f);
-		gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+		// Back Face
+		gl.glTexCoord2f(textureRight, textureBottom);
 		gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+		gl.glTexCoord2f(textureRight, textureTop);
+		gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureTop);
+		gl.glVertex3f(1.0f, 1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureBottom);
 		gl.glVertex3f(1.0f, -1.0f, -1.0f);
 
-		// Front-face
-		gl.glColor3f(1.0f, 0.0f, 0.0f); // red
-		gl.glVertex3f(1.0f, 1.0f, 1.0f);
-		gl.glVertex3f(-1.0f, 1.0f, 1.0f);
-		gl.glVertex3f(-1.0f, -1.0f, 1.0f);
-		gl.glVertex3f(1.0f, -1.0f, 1.0f);
-
-		// Back-face
-		gl.glColor3f(1.0f, 1.0f, 0.0f); // yellow
-		gl.glVertex3f(1.0f, -1.0f, -1.0f);
-		gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+		// Top Face
+		gl.glTexCoord2f(textureLeft, textureTop);
 		gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureBottom);
+		gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+		gl.glTexCoord2f(textureRight, textureBottom);
+		gl.glVertex3f(1.0f, 1.0f, 1.0f);
+		gl.glTexCoord2f(textureRight, textureTop);
 		gl.glVertex3f(1.0f, 1.0f, -1.0f);
 
-		// Left-face
-		gl.glColor3f(0.0f, 0.0f, 1.0f); // blue
-		gl.glVertex3f(-1.0f, 1.0f, 1.0f);
-		gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+		// Bottom Face
+		gl.glTexCoord2f(textureRight, textureTop);
 		gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureTop);
+		gl.glVertex3f(1.0f, -1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureBottom);
+		gl.glVertex3f(1.0f, -1.0f, 1.0f);
+		gl.glTexCoord2f(textureRight, textureBottom);
 		gl.glVertex3f(-1.0f, -1.0f, 1.0f);
 
-		// Right-face
-		gl.glColor3f(1.0f, 0.0f, 1.0f); // magenta
-		gl.glVertex3f(1.0f, 1.0f, -1.0f);
-		gl.glVertex3f(1.0f, 1.0f, 1.0f);
-		gl.glVertex3f(1.0f, -1.0f, 1.0f);
+		// Right face
+		gl.glTexCoord2f(textureRight, textureBottom);
 		gl.glVertex3f(1.0f, -1.0f, -1.0f);
+		gl.glTexCoord2f(textureRight, textureTop);
+		gl.glVertex3f(1.0f, 1.0f, -1.0f);
+		gl.glTexCoord2f(textureLeft, textureTop);
+		gl.glVertex3f(1.0f, 1.0f, 1.0f);
+		gl.glTexCoord2f(textureLeft, textureBottom);
+		gl.glVertex3f(1.0f, -1.0f, 1.0f);
+
+		// Left Face
+		gl.glTexCoord2f(textureLeft, textureBottom);
+		gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+		gl.glTexCoord2f(textureRight, textureBottom);
+		gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+		gl.glTexCoord2f(textureRight, textureTop);
+		gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+		gl.glTexCoord2f(textureLeft, textureTop);
+		gl.glVertex3f(-1.0f, 1.0f, -1.0f);
 
 		gl.glEnd();
 		gl.glEndList();
-		
+
 		return cubeDList;
+	}
+
+	public void loadTexture(GL2 gl) {
+		// Load texture from image
+		try {
+			// Create a OpenGL Texture object from (URL, mipmap, file suffix)
+			// Use URL so that can read from JAR and disk file.
+			texture = TextureIO.newTexture(getClass().getClassLoader()
+					.getResource(textureFileName), // relative to project root
+					false, textureFileType);
+
+			// Use linear filter for texture if image is larger than the
+			// original texture
+			gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			// Use linear filter for texture if image is smaller than the
+			// original texture
+			gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+			// Texture image flips vertically. Shall use TextureCoords class to
+			// retrieve
+			// the top, bottom, left and right coordinates, instead of using
+			// 0.0f and 1.0f.
+			TextureCoords textureCoords = texture.getImageTexCoords();
+			textureTop = textureCoords.top();
+			textureBottom = textureCoords.bottom();
+			textureLeft = textureCoords.left();
+			textureRight = textureCoords.right();
+		} catch (GLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void buildTerrain(GL2 gl, int displayList) {
+
+		for (int i = 0; i < 5; i++) {
+
+			gl.glPushMatrix();
+
+			gl.glTranslatef(i * 3f, 0.0f, -6.0f);
+
+			gl.glColor3fv(boxColors[2], 0);
+
+			if (i == 1) {
+				tempRotX += 2f;
+				gl.glRotatef(tempRotX, 1.0f, 0.0f, 0.0f);
+			}
+
+			gl.glCallList(displayList); // draw the cube
+			gl.glPopMatrix();
+		}
 	}
 }
