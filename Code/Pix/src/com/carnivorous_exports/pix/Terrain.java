8d8ba6c04376a21a3b64c1909d73a52788fbc59b
Nate Schultz <nate.schultz@outlook.com>
2013-12-12 11:25:13 -0500

    moved Quad to a seperate class

--- Code/Pix/src/com/carnivorous_exports/pix/Terrain.java
+++ Code/Pix/src/com/carnivorous_exports/pix/Terrain.java
@@ -30,9 +30,9 @@ public class Terrain {
 	//private boolean[][][][] placement = new boolean[4][12][12][12];
 	private ArrayList[][] coords = new ArrayList[3][3];
 	
-	boolean[][][][] place1;
-	boolean[][][][] place2;
-	boolean[][][][] place3;
+	Quad place1;
+	Quad place2;
+	Quad place3;
 
 	private float[] treeSize;
 
@@ -186,9 +186,9 @@ public class Terrain {
 		}
 		*/
 		
-			place1 = (initQuad(12, 8, 12, 0.3f));
-			place2 = (initQuad(12, 8, 12, 0.3f));
-			place3 = (initQuad(12, 8, 12, 0.3f));
+		place1 = new Quad(12, 8, 12, 0.5f);
+		place2 = new Quad(12, 8, 12, 0.5f);
+		place3 = new Quad(12, 8, 12, 0.5f);
 		
 	}
 
@@ -203,9 +203,9 @@ public class Terrain {
 		*/
 		
 		
-		refreshQuad(gl, displayList, place1, 12, 8, 12, 12, 0, -24);
-		refreshQuad(gl, displayList, place2, 12, 8, 12, 0, 0, -24);
-		refreshQuad(gl, displayList, place3, 12, 8, 12, -12, 0, -24);
+		place1.refreshQuad(gl, displayList, 12, 8, 12, 12, 0, -24);
+		place2.refreshQuad(gl, displayList, 12, 8, 12, 0, 0, -24);
+		place3.refreshQuad(gl, displayList, 12, 8, 12, -12, 0, -24);
 		
 		/*
 		refreshQuad(gl, displayList, place1, 12, 8, 12, 0, 0, -12);
@@ -218,119 +218,6 @@ public class Terrain {
 		*/
 		
 		
-	}
-	
-	public boolean[][][][] initQuad(int topX, int topY, int topZ, float variety) {
-		//topX, Y, and Z are the boundaries of the quad, when they are 12 it is a cube
-		//variety is the variety of cube sizes: 
-		//		0.0 is entirely size 1 cubes, 1.0 is entirely size 4 cubes
-		
-		//boolean[][][] doesNotFit = new boolean[12][12][12]; 	// is false when
-												// [x][y][z] space is
-												// occupied
-		
-		boolean[][][][] placement = new boolean[4][12][12][12];
-		boolean[][][] doesNotFit = new boolean[12][12][12];
-		
-		boolean fitting = true; // is true when the block will be placed
-
-		for (int i = 4; i >= 1; i--) { // loop for each box size
-			for (int x = 0; x < topX; x++) { // x coords
-				for (int y = 0; y < topY; y++) { // y coords
-					for (int z = 0; z < topZ; z++) { // z coords
-
-						// randomize
-						if (i != 1 && (float) Math.random() < variety) {
-							fitting = false;
-						} else {
-
-							// determine if that area is occupied
-							for (int x2 = (i - 1) + x; x2 >= x; x2--) {
-								for (int y2 = (i - 1) + y; y2 >= y; y2--) {
-									for (int z2 = (i - 1) + z; z2 >= z; z2--) {
-
-										// this if statement lets some blocks
-										// pop out of the top
-										if (x2 < 12 && y2 < 12 && z2 < 12) {
-											if (doesNotFit[x2][y2][z2])
-												fitting = false;
-
-										} else
-											fitting = false;
-									}
-								}
-							}
-
-						}
-
-						if (fitting) {
-
-							// declare that area as occupied
-							for (int x2 = (i - 1) + x; x2 >= x; x2--) {
-								for (int y2 = (i - 1) + y; y2 >= y; y2--) {
-									for (int z2 = (i - 1) + z; z2 >= z; z2--) {
-										doesNotFit[x2][y2][z2] = true;
-									}
-								}
-							}
-
-							// remember placement
-							placement[i - 1][x][y][z] = true;
-						} //else placement[i - 1][x][y][z] = false;
-
-						// reset fitting
-						fitting = true;
-					}
-				}
-			}
-		}
-		
-		return placement;
-	}
-	
-	public void refreshQuad(GL2 gl, int[] displayList, boolean[][][][] place, 
-			int topX, int topY, int topZ, int posX, int posY, int posZ) {
-
-		for (int i = 4; i >= 1; i--) { // loop for each box size
-			for (int x = 0; x < topX; x++) { // x coords
-				for (int y = 0; y < topY; y++) { // y coords
-					for (int z = 0; z < topZ; z++) { // z coords
-
-						// if a box is placed there
-						if (place[i - 1][x][y][z]) {
-
-							// draw the box
-							gl.glPushMatrix();
-
-							gl.glScalef(0.25f * i, 0.25f * i, 0.25f * i);
-
-							// move to the coordinate
-							if (i == 4) {
-								gl.glTranslatef(.5f * (x + posX),
-										.5f * (y + posY), .5f * (z + posZ));
-							} else if (i == 3) {
-								gl.glTranslatef(0.6666f * (x + posX) - 0.3333f,
-										0.6666f * (y + posY) - 0.3333f,
-										0.6666f * (z + posZ) - 0.3333f);
-							} else if (i == 2) {
-								gl.glTranslatef(1f * (x + posX) - 1f,
-										1f * (y + posY) - 1f,
-										1f * (z + posZ) - 1f);
-							} else if (i == 1) {
-								gl.glTranslatef(2f * (x + posX) - 3f,
-										2f * (y + posY) - 3f,
-										2f * (z + posZ) - 3f);
-							}
-
-							// draw the cube
-							gl.glCallList(displayList[0]);
-
-							gl.glPopMatrix();
-						}
-					}
-				}
-			}
-		}
 	}
 
 	// remove posNum if numArrays works
