a2d885fb634cae23ab62c9cdfb3eed311a951d8d
Nate Schultz <nate.schultz@outlook.com>
2014-05-07 22:56:08 -0400

    got regular cube to rectangle collision working

--- CarnivorousEngine/src/com/carnivorous_exports/engine/Renderer.java
+++ CarnivorousEngine/src/com/carnivorous_exports/engine/Renderer.java
@@ -131,7 +131,8 @@ public class Renderer implements GLEventListener,
 	
 	float[] cube1 = {-8f,0f,-4f};
 	float[] cube2 = {6f,0f,-4f};
-	float[][] cubeArray = {cube1, cube2};
+	float[] cube3 = {0f,1f,-10f};
+	float[][] cubeArray = {cube1, cube2, cube3};
 
 	/**
 	 * A very simple constructor. All it does is add the event listeners to
